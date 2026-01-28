8d7abba79eab3e7e9854ee4a8d2ba4acb29b548a
fnkim <fannykim42@gmail.com>
2026-01-27 18:03:48 -0800

    in class 4

--- GDIM32 In-Class Activities/Assets/W4/Scripts/W4Pigeon.cs
+++ GDIM32 In-Class Activities/Assets/W4/Scripts/W4Pigeon.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public class W4Pigeon : MonoBehaviour
 {
+    
     [SerializeField] private AudioSource _audio;
     [SerializeField] private Animator _animator;
 
-    // REMOVE these references to other objects!
-    // we're going to alert them via EVENT instead!!
-    [SerializeField] private W4Seagull[] _seagulls;
-    [SerializeField] private W4UI _ui;
-    [SerializeField] private W4VFX _vfx;
 
     // HERE, add an event to tell other objects that the pigeon coo'd!
-
+    public delegate void CooDelegate();
+        public event CooDelegate PigeonCoo;
     // don't change the code in this method!
     void Update()
     {
@@ -34,15 +31,6 @@ public class W4Pigeon : MonoBehaviour
         // instead, fire your coo event!
         
         // tell seagulls
-        foreach(W4Seagull seagull in _seagulls)
-        {
-            seagull.HandlePigeonCoo();
-        }
-
-        // tell UI
-        _ui.HandlePigeonCoo();
-
-        // tell VFX
-        _vfx.HandlePigeonCoo();
+        PigeonCoo?.Invoke();
     }
 }
